6e015ef27b260d1ab0f0c3dfacd796fee4cbe137
Akallabet <grazianos886@gmail.com>
2018-09-09 12:15:32 +0100

    use only xe currency data

--- src/utils.js
+++ src/utils.js
@@ -7,31 +7,30 @@ const roundDecimals = decimals => number => Math.round(number * decimals) / deci
 export const roundToFourDecimals = roundDecimals(10000)
 export const roundToTwoDecimals = roundDecimals(100)
 
-const EURRatesUrl = 'http://data.fixer.io/api/latest?access_key=2c8a5f55ce59893e49e04d7703465b22&symbols=USD,GBP&format=1'
-const USDRatesUrl = 'http://apilayer.net/api/live?access_key=aeaa8a104093e098b1c4ae7296b8ce25&base=GBP&currencies=GBP,EUR&format=1'
-const GBPRatesUrl = 'https://free.currencyconverterapi.com/api/v6/convert?q=GBP_USD,GBP_EUR&compact=ultra'
+const instance = axios.create({
+  baseURL: 'https://xecdapi.xe.com/v1/historic_rate.json/',
+  timeout: 1000,
+  headers: {'Authorization': 'Basic bXIuODEyNTUzNzkyOnFybHIxanFiYWJvOWZvczNxYzlsdjFoZjZ2'}
+})
 
-const fetchUSDRates = () => axios.get(USDRatesUrl)
-  .then(({data}) => {
-    const { quotes: { USDGBP, USDEUR } } = data
-    return {
-      [CURRENCY_CODES.GBP]: roundToFourDecimals(USDGBP),
-      [CURRENCY_CODES.EUR]: roundToFourDecimals(USDEUR)
-    }
+const extractRates = currencies => ({data: {to}}) => {
+  const rates = {}
+  currencies.forEach(currency => {
+    rates[currency] = roundToFourDecimals(to.find(d => d.quotecurrency === currency).mid)
   })
-const fetchEURRates = () => axios.get(EURRatesUrl)
-  .then(({data}) => {
-    const {rates} = data
-    return {
-      [CURRENCY_CODES.GBP]: roundToFourDecimals(rates[CURRENCY_CODES.GBP]),
-      [CURRENCY_CODES.USD]: roundToFourDecimals(rates[CURRENCY_CODES.USD])
-    }
-  })
-const fetchGBPRates = () => axios.get(GBPRatesUrl)
-  .then(({data}) => ({
-    [CURRENCY_CODES.EUR]: roundToFourDecimals(data['GBP_EUR']),
-    [CURRENCY_CODES.USD]: roundToFourDecimals(data['GBP_USD'])
-  }))
+  return rates
+}
+
+const extractEURGBPRates = extractRates([CURRENCY_CODES.EUR, CURRENCY_CODES.GBP])
+const extractUSDGBPRates = extractRates([CURRENCY_CODES.USD, CURRENCY_CODES.GBP])
+const extractEURUSDRates = extractRates([CURRENCY_CODES.EUR, CURRENCY_CODES.USD])
+
+const fetchUSDRates = () => instance.get('?from=USD&to=EUR,GBP')
+  .then(extractEURGBPRates)
+const fetchEURRates = () => instance.get('?from=EUR&to=USD,GBP')
+  .then(extractUSDGBPRates)
+const fetchGBPRates = () => instance.get('?from=GBP&to=EUR,USD')
+  .then(extractEURUSDRates)
 
 export const fetchRates = () => Promise.all([
   fetchUSDRates(),
